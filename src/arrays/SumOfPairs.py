e8320ac4218780a7a739f2dd0055bd51e1d73796
Narayana Rallabandi <narayana.rallabandi@oracle.com>
2019-08-01 21:51:52 +0530

    Sum Of Pairs in a sorted array with O(n) time

---
+++ src/arrays/SumOfPairs.py
@@ -0,0 +1,19 @@
+
+def sumOfPairs(nums,sum):
+    first = 0
+    last = len(nums)-1
+    
+    found = False
+    
+    while not found and first<last:
+        if nums[first] + nums[last] == sum:
+            found = True
+        elif nums[first] + nums[last] > sum:
+            last -= 1
+        else:
+            first += 1   
+            
+    print("found at index {}, {}".format(first,last))             
+    
+nums =  [3, 5, 5, 10, 15, 17, 20 ]
+sumOfPairs(nums, 20)  
